cb332045283b0a6724283eacc34416788907110b
Topias <mactopias@gmail.com>
2020-09-11 11:50:09 +0300

    initial commit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const socket = io("http://localhost:3000");
+const socket = io.connect('http://85.131.19.224:3000');
 const messageContainer = document.getElementById("message-container");
 const messageForm = document.getElementById("send-container");
 const messageInput = document.getElementById("message-input");
